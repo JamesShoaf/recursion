0c1e25ee2b8d82ca75018a9929a3efb4861d63fe
James <IEatPie@github.com>
2020-01-26 20:57:03 -0800

    implement getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var result = [];
+
+  var nodeTrace = function(node, className){
+    if (_.contains(node.classList, className)){
+      result.push(node);
+    }
+    if (node.childNodes){
+      for (let i = 0; i < node.childNodes.length; i++){
+        nodeTrace(node.childNodes[i], className);
+      }
+    }
+  }
+  nodeTrace(document.body, className);
+  return result;
 };
